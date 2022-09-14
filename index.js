9375575a5671dc67804f9c4e71c21c993f71d48d
Govind Lowanshi <govind.lowanshi28@gmail.com>
2022-09-14 20:29:54 +0530

    add all the file

---
+++ index.js
@@ -0,0 +1,34 @@
+const fs = require('fs/promises')
+
+const myFileWriter = async (fileName, fileContent) => {
+	// write code here
+	// dont chnage function name
+	fs.writeFile(fileName , fileContent)
+}
+
+const myFileReader = async (fileName) => {
+	// write code here
+	// dont chnage function name
+	let data = fs.readFile(fileName,(err,data)=>{
+		return data
+	})
+	console.log((await data).toString())
+}
+
+const myFileUpdater = async (fileName, fileContent) => {
+	// write code here
+	// dont chnage function name
+	fs.appendFile(fileName , fileContent)
+}
+
+const myFileDeleter = async (fileName) => {
+	// write code here
+	// dont chnage function name
+	fs.unlink(fileName);
+}
+myFileWriter("GovindFile.txt" , "hello")
+// myFileReader("GovindFile.txt")
+// myFileUpdater("GovindFile.txt" , " World");
+// myFileDeleter("GovindFile.txt")
+
+module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
